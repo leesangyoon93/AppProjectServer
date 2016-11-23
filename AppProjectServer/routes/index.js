0a3b18f79538a27c5e989345972baeb03a1d12ac
leesangyoon93 <libertyoon@naver.com>
2016-11-23 20:57:03 +0900

    asdf

--- AppProjectServer/routes/index.js
+++ AppProjectServer/routes/index.js
@@ -875,11 +875,11 @@ router.post("/saveCategoryState", function (req, res) {
                             var tmp = "state" + i.toString();
                             console.log(category.custom[i-9].content);
                             if(req.body[tmp] == "true") {
-                                category.custom[i-9]['state'] = true;
+                                category.custom[i-9].state.update(true);
                                 category.save();
                             }
                             else {
-                                category.custom[i-9]['state'] = false;
+                                category.custom[i-9].state.update(false);
                                 category.save();
                             }
 
