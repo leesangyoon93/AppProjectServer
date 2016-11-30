c4cf1dc573cc909a5814b0a43b1a418312c81377
leesangyoon93 <libertyoon@naver.com>
2016-12-01 00:28:35 +0900

    asdf

--- AppProjectServer/routes/index.js
+++ AppProjectServer/routes/index.js
@@ -1043,9 +1043,9 @@ router.post('/addCategory', function (req, res) {
 
 Patient.find(function (err, patients) {
     var date = new Date();
-    var dateNow = date.getFullYear() + "-" + date.getMonth()+1 + "-" + date.getDate();
+    var dateNow = date.getFullYear().toString() + "-" + date.getMonth().toString() + "-" + date.getDate().toString();
     var yesterday = new Date(date.valueOf() - (24*60*60*1000));
-    var dateYesterday = yesterday.getFullYear() + "-" + yesterday.getMonth()+1 + "-" + yesterday.getDay();
+    var dateYesterday = yesterday.getFullYear().toString() + "-" + yesterday.getMonth().toString()+1 + "-" + yesterday.getDay().toString();
     console.log(dateNow);
     console.log(dateYesterday);
     Category.find({date: dateNow}, function (err, tmps) {
